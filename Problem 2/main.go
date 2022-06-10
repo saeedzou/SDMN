7019eebdf937f8a3a06b84731e5bec58e9a3c7cc
Saeedreza Zouashkiani <saeedzou2012@gmail.com>
2022-06-10 16:40:37 +0430

    Updated main.go to isolate network namespace

--- Problem 2/main.go
+++ Problem 2/main.go
@@ -1,3 +1,14 @@
+/**
+	This program creates a container with isolated net, mount, uts, and pid namespaces
+	and runs bash in the container.
+	To run another script when starting the container change the argument of line 59 
+	from "/bin/bash" to your arbitary script
+	Instructions to run this program:
+		1.	Go to the directory where this file is at
+		2.	Run this program with sudo privelage: >> go run main.go <hostname> [-m || --memory] <memory_limit>
+		3.	The memory option is optional and limits the memory usage of the container in megabytes
+
+*/
 package main
 
 import (
@@ -7,6 +18,7 @@ import (
 	"syscall"
 	"path/filepath"
 	"io/ioutil"
+	"strconv"
 )
 func main() {
 	switch os.Args[1] {
@@ -26,20 +38,24 @@ func run() {
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+	// Cloning uts, pid, mount, and net namespaces
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS ,
+		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWNET,
 		Unshareflags: syscall.CLONE_NEWNS ,
 	}
 	cmd.Run()
 	
 
 }
+//  Add a child process so that we can change hostname and mount filesystem and create cgroups 
 func child() {
 	fmt.Printf("Running %v %d\n", os.Args[2:], os.Getpid())
-	fmt.Printf("%d\n",len(os.Args))
-	if (os.Args[3]=="--memory" || os.Args[3]=="-m"){
-		cg(os.Args[4])
-	}
+	// optional arguement to limit memory usage of the container
+	if len(os.Args)==5 {
+		if (os.Args[3]=="--memory" || os.Args[3]=="-m"){
+			cg(os.Args[4])
+		}
+	}	
 	syscall.Sethostname([]byte(os.Args[2]))
 	syscall.Chroot("./ubuntu_fs")
 	syscall.Chdir("/")
@@ -61,6 +77,8 @@ func cg(memory_limit string) {
 	memory := filepath.Join(cgroups, "memory")
 	os.Mkdir(filepath.Join(memory, "container"), 0755)
 	must(ioutil.WriteFile(filepath.Join(memory, "container/memory.limit_in_bytes"), []byte(memory_limit+"M"), 0700))
+	//  assigning the process id of the container to the tasks of cgroup container
+	must(ioutil.WriteFile(filepath.Join(memory, "container/tasks"), []byte(strconv.Itoa(os.Getpid())), 0700))
 }
 
 
